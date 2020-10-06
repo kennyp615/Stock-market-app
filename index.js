4a5fae8380e88b3a48684dd7ceaac91cb1ea4c87
Kenneth Pruitt <kennethpruitt3@gmail.com>
2020-10-06 08:51:12 -0500

    finishing up index.js

--- index.js
+++ index.js
@@ -1,35 +1,69 @@
+//my stock market app
 const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
 const request = require('request')
+const bodyParser = require('body-parser');
 
 const PORT = process.env.PORT || 5000;
 
-//Set handlebar middleware here
+
+// use body parser middleware
+app.use(bodyParser.urlencoded({extended: false}));
+
+
+// API key pk_14ed8241ef3a4c45b50938dc759d1375
+// create call_api function
+function call_api(finishedAPI, ticker) {
+	request('https://cloud.iexapis.com/stable/stock/' + ticker + '/quote?token=pk_14ed8241ef3a4c45b50938dc759d1375', { json: true }, (err, res, body) => {
+	if (err) {return console.log(err);}
+	if (res.statusCode === 200){
+		//console.log(body);
+		finishedAPI(body);
+		};
+	});
+};
+
+
+// Set Handlebars Middleware
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
-// API key pk_14ed8241ef3a4c45b50938dc759d1375
-request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_14ed8241ef3a4c45b50938dc759d1375', { json: true }, (err, res, body) => {
-    if (err) {return console.log(err);}
-    console.log(body);
-    if (res.statusCode === 200) {
-        console.log(body);
-    };
-});
+const otherstuff = "hello there, this is other stuff!";
 
-// set routes for handlebars
+// Set handlebar index GET route
 app.get('/', function (req, res) {
-    res.render('home');
+	call_api(function(doneAPI) {
+			res.render('home', {
+	    	stock: doneAPI,
+    	});
+	}, "fb");
+		
 });
 
-// creating routes for about handlebars
+// Set handlebar index POST route
+app.post('/', function (req, res) {
+	call_api(function(doneAPI) {
+			//console.log(doneAPI);
+			//posted_stuff = req.body.stock_ticker;
+			res.render('home', {
+	    	stock: doneAPI,
+    	});
+	}, req.body.stock_ticker);
+		
+});
+
+
+
+
+// create about page route
 app.get('/about.html', function (req, res) {
     res.render('about');
 });
 
-// create a static folder
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT, () => console.log('Server Listening on port' + PORT));
+
+app.listen(PORT, () => console.log('Server Listening on port ' + PORT));
