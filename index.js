8e20adb79a1d7941da04a8e71c8d9dd860ce4283
Kenneth Pruitt <kennethpruitt3@gmail.com>
2020-10-05 19:44:21 -0500

    added api key

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const exphbs  = require('express-handlebars');
 const path = require('path');
+const request = require('request')
 
 const PORT = process.env.PORT || 5000;
 
@@ -9,9 +10,23 @@ const PORT = process.env.PORT || 5000;
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
+// API key pk_14ed8241ef3a4c45b50938dc759d1375
+request('https://cloud.iexapis.com/stable/stock/fb/quote?token=pk_14ed8241ef3a4c45b50938dc759d1375', { json: true }, (err, res, body) => {
+    if (err) {return console.log(err);}
+    console.log(body);
+    if (res.statusCode === 200) {
+        console.log(body);
+    };
+});
+
 // set routes for handlebars
 app.get('/', function (req, res) {
     res.render('home');
+});
+
+// creating routes for about handlebars
+app.get('/about.html', function (req, res) {
+    res.render('about');
 });
 
 // create a static folder
